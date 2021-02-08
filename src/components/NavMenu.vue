e8d41be54bbbcbbd2576de5030c61ccf38e375ba
shaws <shawsxian@gmail.com>
2021-02-08 19:11:57 +0800

    编写overview

---
+++ src/components/NavMenu.vue
@@ -0,0 +1,53 @@
+<template>
+  <el-row class="tac">
+    <el-col :span="30">
+      <el-menu
+        default-active="2"
+        class="el-menu-vertical-demo"
+        @open="handleOpen"
+        @close="handleClose"
+      >
+        <el-menu-item index="1" v-on:click="speedMismatch">
+          <i class="el-icon-menu"></i>
+          <span slot="title">终端速率不匹配（万）</span>
+        </el-menu-item>
+        <el-menu-item index="2">
+          <i class="el-icon-menu"></i>
+          <span slot="title">光宽+ITV全程转化率</span>
+        </el-menu-item>
+        <el-menu-item index="3">
+          <i class="el-icon-menu"></i>
+          <span slot="title">宽带1小时预约率</span>
+        </el-menu-item>
+        <el-menu-item index="4">
+          <i class="el-icon-menu"></i>
+          <span slot="title">宽带2小时装机</span>
+        </el-menu-item>
+        <el-menu-item index="5">
+          <i class="el-icon-menu"></i>
+          <span slot="title">改约率</span>
+        </el-menu-item>
+        <el-menu-item index="6">
+          <i class="el-icon-menu"></i>
+          <span slot="title">测速达标率</span>
+        </el-menu-item>
+      </el-menu>
+    </el-col>
+  </el-row>
+</template>
+<script>
+  export default {
+    methods: {
+      handleOpen(key, keyPath) {
+        console.log(key, keyPath)
+      },
+      handleClose(key, keyPath) {
+        console.log(key, keyPath)
+      },
+      speedMismatch(){
+        alert('11111')
+      }
+
+    }
+  }
+</script>
--- src/components/Overview.vue
+++ src/components/Overview.vue
@@ -1,24 +1,24 @@
 <template>
   <div id="overview">
-      <test-element>hahaha</test-element>
+      <el-container>
+      <el-header>装维室数据展示</el-header>
+      <el-container>
+        <el-aside width="300px">
+          <navmenu></navmenu>
+        </el-aside>
+        <el-main>Main</el-main>
+      </el-container>
+    </el-container>
   </div>
 </template>
 <script>
-import TestElement from './TestElement';
-    export default {
 
-      components: {
-        TestElement
-      },
+import NavMenu from '../components/NavMenu'
 
-      props: {
-
-      },
-
-      data() {
-        return {
-
-        }
-      }
+export default {
+  name: 'app',
+  components: {
+    'navmenu': NavMenu
+  }
 }
 </script>
